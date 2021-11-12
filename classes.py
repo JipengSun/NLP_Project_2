69a752473e762cf0b539af25d80d300c084f8652
joshualevitas <jnl@u.northwestern.edu>
2021-11-12 14:21:14 -0600

    renamed parts of ingredients class

--- classes.py
+++ classes.py
@@ -5,12 +5,14 @@ class Ingredient:
     def __init__(self, ingredient):
         self.name = ingredient
         self.quantity = 0
-        self.measurement = ""
+        self.unit = ""
         # self.descriptor = ""
         # self.preparation = ""
 
     def set_quantity(self, q):
         self.quantity = q
 
-    def set_measurement(self, m):
-        self.measurement = m
+    def set_unit(self, m):
+        self.unit = m
+
+    
