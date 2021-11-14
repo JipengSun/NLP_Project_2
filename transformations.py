67ed6719845cba9363018fd552434290b78a2e9d
joshualevitas <jnl@u.northwestern.edu>
2021-11-14 13:17:28 -0600

    small update

---
+++ transformations.py
@@ -0,0 +1,18 @@
+#tranformations
+
+def make_vegetarian(recipe):
+    #replace meats with vegetarian alternatives
+
+    return 
+
+def make_healthy(recipe):
+    #replace unhealthy foods with healthy alternatives
+    #replace frying with alternative method
+    
+    return 
+
+def make_indian(recipe):
+    #remove all spices, replace with indian spices
+    #[remove certain meats, replace with others?]
+
+    return 
