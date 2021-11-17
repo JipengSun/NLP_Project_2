d4dc07ecfcfe4b97e490f57f559a16b93b58e043
joshualevitas <jnl@u.northwestern.edu>
2021-11-17 13:28:26 -0600

    added optional transformation backbones

--- transformations.py
+++ transformations.py
@@ -15,4 +15,26 @@ def make_indian(recipe):
     #remove all spices, replace with indian spices
     #[remove certain meats, replace with others?]
 
-    return 
+    #dictionary
+
+    #no beef, use spices (red chile powder [paste], curry powder, masala)
+    #mostly eat chicken
+
+    return
+
+def make_kosher(recipe):
+    return
+
+def make_gluten_free(recipe):
+    return
+
+def make_dairy_free(recipe):
+    return
+
+
+### optional
+def scale_recipe(recipe, scale):
+    # scale (float to multiply amounts by)
+    return
+
+ 
