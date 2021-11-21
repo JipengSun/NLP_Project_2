ce0af6d8ca288ab8cff206b2bc3f1f87d8dd0a95
Lukas <lukasbill@163.com>
2021-11-21 13:27:47 -0600

    Update the ingredient list

--- transformations.py
+++ transformations.py
@@ -1,5 +1,6 @@
 #tranformations
 import json
+import nltk
 from nltk.probability import DictionaryConditionalProbDist
 import get_recipe_json
 
@@ -72,30 +73,90 @@ def make_vegetarian(recipe_url):
 def make_healthy(recipe_data):
     #replace unhealthy foods with healthy alternatives
     #replace frying with alternative method
-    sugar = []
-    unhealthy_oil = []
-    trans_fat = ['vegetable oil','fried food','chips','creamer','margarine']
-    saturated_fat = ['cheese','coconut oil','whole milk','red meat']
-    salt = []
+
+    category_reason = {
+        "trans_fat":"The original recipe contains too much trans-fat, which may add your chance to get inflammation.",
+        "saturated_fat":"The original recipe contains saturated fat, which may cause artery blockage.",
+        "massive_sugar":"The massive amount of added sugar may higher the chance of obesity",
+        "salt":"Excessive sodium consumption is the key negetive factor of longevity.",
+        "vegetables":"The original recipe doesn't contain vegetables which provides you essential Vitamins and fiber, consider adding some vegetables in your recipe",
+        "fruit":"There is no fruit in your recipe but that is common, don't forget to eat some fruits after the meal!",
+        "grains":"Grains are naturally high in fiber, helping you feel full and satisfied — which makes it easier to maintain a healthy body weight.",
+        "protein":"The original recipe doesn't contain enough protein which is helpful for muscle gain and health."
+    }
+    massive_sugar = ['ice cream','candy','pastries','cookies','soda','fruit juices','canned fruit','processed meat','breakfast cereals','ketchup','beet sugar',\
+        'blackstrap molasses','brown sugar','buttered syrup','cane juice crystals','cane sugar','caramel','carob syrup','castor sugar',\
+            'coconut sugar','powdered sugar','date sugar','demerara sugar','Florida crystals','fruit juice','fruit juice concentrate','golden sugar',\
+                'golden syrup','grape sugar','honey','icing sugar','invert sugar','maple syrup','molasses','muscovado sugar','panela sugar','rapadura',\
+                    'raw sugar','refiner’s syrup','sorghum syrup','sucanat','treacle sugar','turbinado sugar','yellow sugar']
+
+    unhealthy_oil = ['soybean oil','corn oil','cottonseed oil','sunflower oil','peanut oil','sesame oil','rice bran oil','flaxseed oil']
+    trans_fat = ['fried food','chips','creamer','margarine']+unhealthy_oil
+
+    red_meat = ['beef','lamb','mutton','pork','veal','venison','goat']
+    processed_meat = ['sausage','bacon','ham','deli meats','salami','pâtés','canned meat','corned beef','luncheon meats','prosciutto']
+    cheese = ['cheese','roquefort','camembert','cotija','chèvre','feta','mozzarella','emmental','cheddar','gouda','taleggio','parmigiano-reggiano','manchego','monterey jack']
+    saturated_fat = ['coconut oil','whole milk'] + cheese + red_meat + processed_meat
+    salt = ['salt']
 
     unhealthy_ingredient_data = {
-        "sugar":[],
-        "fat":[],
-        "red meat":[]
+        "massive_sugar":massive_sugar,
+        "trans_fat":trans_fat,
+        "saturated_fat":saturated_fat,
+        "salt" : salt
     }
 
+
     vegetables = []
+    with open('vegetable_list.txt') as f:
+        line = f.readline()
+        while line:
+            line = f.readline()
+            vegetables.append(line.strip().lower())
+    #print(vegetables)
     fruits = []
+    with open('fruit_list.txt') as f:
+        line = f.readline()
+        while line:
+            line = f.readline()
+            fruits.append(line.strip().lower())
+    #print(fruits)
     grains = ['quinoa','corn','millet','brown rice']
-    protein = ['lean meat','eggs','nuts','seeds','soy products']
-    healthy_oil = ['olive','flaxseed','canola','aocado']
+    healthy_oil = ['olive oil','canola oil','aocado oil','walnut oil']
+    white_meat = ['chicken','turkey','duck','goose','game birds','rabbit','pheasant']
+    fish_meat = ['salmon','cod','herring','mahi-mahi','mackerel','perch','rainbow trout','sardines','striped bass','tuna','alaskan pollock','char']
+    soy_products = []
+    with open('soy_product_list.txt') as f:
+        line = f.readline()
+        while line:
+            line = f.readline()
+            soy_products.append(line.strip().lower())
+    #print(soy_products)
+
+    legume_list = []
+    with open('legume_list.txt') as f:
+        line = f.readline()
+        while line:
+            line = f.readline()
+            legume_list.append(line.strip().lower())
+    #print(legume_list)
+
+    protein = ['egg']+legume_list+soy_products+red_meat+white_meat+fish_meat
 
     healthy_ingredient_data = {
-        "vegetables"
+        'vegetables':vegetables,
+        'fruits':fruits,
+        'grains':grains,
+        'protein':protein,
+        'healthy_oil':healthy_oil,
+        'white_meat':white_meat,
+        'fish_meat':fish_meat
     }
 
     for step in recipe_data['steps']:
-        print (step)
+        a = nltk.word_tokenize(step)
+        #print(a)
+        
         
 
 
